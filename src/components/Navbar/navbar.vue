d83aacc52b563497e0261f1d8398f3ca96fd9478
sehrishayaz25 <sehrishayaz25@gmail.com>
2021-08-01 18:53:41 +0500

    Catch Technology

---
+++ src/components/Navbar/navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar">
+    <div>
+      <img src="src/assets/ct.png" alt="" />
+    </div>
+    <div>
+      <a href="#home">Home</a>
+      <a href="#news">Abou Us</a>
+      <a href="#contact">Our Services</a>
+      <a href="#contact">Projects</a>
+      <a href="#contact">Contacts</a>
+    </div>
+  </div>
+</template>
+
